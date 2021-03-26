3c28f8b9f0be99765f4dcf07dd06a5e22e9c6285
Javier Catalá <javicatala@iesdoctorbalmis.com>
2021-03-26 10:22:55 +0100

    first commit

---
+++ tema2_face/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tema2_face
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
